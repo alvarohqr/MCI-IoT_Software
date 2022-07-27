13bf137f4ec45778ef997294f2feb6d645685a67
alvarohqr <55456864+alvarohqr@users.noreply.github.com>
2022-07-27 13:08:29 -0700

    Update aggregation.py

--- aggregation.py
+++ aggregation.py
@@ -1,3 +1,8 @@
+#Obtener y restaurar en MongoDB el archivo students.json (adjunto) y realizar los pipelines necesarios para obtener la siguiente información:
+    #Calificación promedio de cada alumno.
+    #Calificación promedio para cada alumno, sólo de exámenes y tareas.
+    #Calificación promedio de todo el grupo sólo de exámenes
+
 import pymongo
 
 client = pymongo.MongoClient("mongodb://localhost:27017/") #Servidor local
